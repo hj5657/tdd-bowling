e9691456e57de4f6affe8bded7901495b97e6d54
hejie <565703319@qq.com>
2020-09-16 12:10:02 +0800

    refotor code

--- src/main/java/BowlingGame/BowlingGame.java
+++ src/main/java/BowlingGame/BowlingGame.java
@@ -4,31 +4,32 @@ package BowlingGame;
  * Create by 木水 on 2020/9/14.
  */
 public class BowlingGame {
-    private int totalScore=0;
-    private int cnt=1;
+    private int totalScore = 0;
+    private int cnt = 0;
+
     public int getTotal() {
         return totalScore;
     }
 
     public void addEachNumber(int[] number) {
-        for (int i = 1; i <number.length ; i++) {
-            if (cnt<10) {
+        for (int i = 1; i < number.length; i++) {
+            if (cnt < 10) {
                 if (number[i] == 10) {
                     cnt++;
                     totalScore += 10;
                     totalScore = totalScore + number[i + 1] + number[i + 2];
-                }
-                if (number[i]+number[i+1]==10){
+                } else if (number[i] + number[i + 1] == 10) {
                     cnt++;
-                    totalScore+=10;
-                    totalScore+=number[i+2];
-                }
-                if (number[i]+number[i+1]<10){
+                    totalScore += 10;
+                    totalScore += number[i + 2];
+                    i++;
+                } else if (number[i] + number[i + 1] < 10) {
                     cnt++;
-                    totalScore=totalScore+number[i]+number[i+1];
+                    totalScore = totalScore + number[i] + number[i + 1];
+                    i++;
                 }
-            }else{
-                totalScore+=number[i];
+            } else {
+                break;
             }
         }
     }
--- src/test/java/twu/BowlingGameTest.java
+++ src/test/java/twu/BowlingGameTest.java
@@ -6,25 +6,35 @@ import org.junit.jupiter.api.Test;
 
 public class BowlingGameTest {
     @Test
-    void should_return_300score_when_all_is_strike(){
-        BowlingGame game=new BowlingGame();
-        int number[]=new int[13];
+    void should_return_300score_when_all_is_strike() {
+        BowlingGame game = new BowlingGame();
+        int number[] = new int[13];
         for (int i = 1; i < 13; i++) {
-            number[i]=10;
+            number[i] = 10;
         }
         game.addEachNumber(number);
-        int result=game.getTotal();
-        Assertions.assertEquals(300,result);
+        int result = game.getTotal();
+        Assertions.assertEquals(300, result);
     }
+
     @Test
-    void should_return_0score_when_all_is_zero(){
-        BowlingGame game=new BowlingGame();
-        int number[]=new int[21];
+    void should_return_0score_when_all_is_zero() {
+        BowlingGame game = new BowlingGame();
+        int number[] = new int[21];
         for (int i = 1; i < number.length; i++) {
-            number[i]=0;
+            number[i] = 0;
         }
         game.addEachNumber(number);
-        int result=game.getTotal();
-        Assertions.assertEquals(0,result);
+        int result = game.getTotal();
+        Assertions.assertEquals(0, result);
+    }
+
+    @Test
+    void should_return_score_when_both_have_strike_and_spare() {
+        BowlingGame game = new BowlingGame();
+        int number[] = {0, 10, 3, 6, 9, 1, 5, 5, 10, 8, 1, 10, 10, 10, 10, 3, 6};
+        game.addEachNumber(number);
+        int result = game.getTotal();
+        Assertions.assertEquals(193, result);
     }
 }
